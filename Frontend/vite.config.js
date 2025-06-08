0a6d0554fe4c3ff0f5ce0e14eb244e8675590049
Aditya-jambhale <jambhaleaditya91@gmail.com>
2025-06-09 00:12:19 +0530

    changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["mammoth"],
-    include: ["pdfjs-dist/build/pdf.worker.entry"]
+    include: ["mammoth", "pdfjs-dist/build/pdf.worker.entry"]
   }
 })
